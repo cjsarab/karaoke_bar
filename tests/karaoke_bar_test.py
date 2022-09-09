0c00d80546710775dadd6d1ac60d876e79e2f8e0
Cammy Smith <cjsarab1991@gmail.com>
2022-09-09 15:31:56 +0100

    Added name tests

--- tests/karaoke_bar_test.py
+++ tests/karaoke_bar_test.py
@@ -20,14 +20,42 @@ class TestKaraokeBar(unittest.TestCase):
         self.song5 = Song("Iron Maiden", "The Number of the Beast", 1982)
         self.song6 = Song("Johnny Cash", "A Boy Named Sue", 1969)
 
-        rooms = []
-        rooms.extend([self.room1, self.room2, self.room3, self.room4, self.room5, self.room6])
+        self.rooms = []
+        self.rooms.extend([self.room1, self.room2, self.room3, self.room4, self.room5, self.room6])
 
-        songs = []
-        songs.extend([self.song1, self.song2, self.song3, self.song4, self.song5, self.song6])
+        self.songs = []
+        self.songs.extend([self.song1, self.song2, self.song3, self.song4, self.song5, self.song6])
 
-        self.karaoke_bar = KaraokeBar("Kode to Joy", 1000, songs, rooms)
+        self.karaoke_bar = KaraokeBar("Kode to Joy", 1000, self.songs, self.rooms)
 
     def test_karaoke_bar_has_name(self):
         self.assertEqual("Kode to Joy", self.karaoke_bar.name) 
+
+    def test_karaoke_bar_has_till_value(self):
+        self.assertEqual(1000, self.karaoke_bar.till) 
+
+    def test_karaoke_bar_has_songs(self):
+        self.assertEqual(self.songs, self.karaoke_bar.songs)
+
+    def test_karaoke_bar_has_rooms(self):
+        self.assertEqual(self.rooms, self.karaoke_bar.rooms)
+
+    def test_karaoke_bar_room_one_joyzone(self):
+        self.assertEqual("Joyzone", self.karaoke_bar.rooms[0].name)
+
+    def test_karaoke_bar_room_two_code_sheeran(self):
+        self.assertEqual("Code Sheeran", self.karaoke_bar.rooms[1].name) 
+
+    def test_karaoke_bar_room_three_house_0_1(self):
+        self.assertEqual("House of the 0 and 1", self.karaoke_bar.rooms[2].name) 
+
+    def test_karaoke_bar_room_four_loop_reed(self):
+        self.assertEqual("Loop Reed", self.karaoke_bar.rooms[3].name) 
+
+    def test_karaoke_bar_room_five_integer_of_the_beast(self):
+        self.assertEqual("The Integer of the Beast", self.karaoke_bar.rooms[4].name) 
+
+    def test_karaoke_bar_room_six_johnny_crash(self):
+        self.assertEqual("Johnny Crash", self.karaoke_bar.rooms[5].name) 
+
 
